3746019677e89a10502c57d7d4b930090e097953
Carlos Sullaez <csullaez@gmail.com>
2026-01-30 11:39:01 -0300

    feat: :rocket: Adicion de datos y servicios mock para facturas

---
+++ src/api/datos.ts
@@ -0,0 +1,35 @@
+import { type Factura } from "../types/factura";
+export const FACTURAS_DATA: Factura[] = [
+  {
+    id: 1,
+    idCliente: "123",
+    servicio: "Agua",
+    periodo: "2025-12",
+    monto: 58.2,
+    estado: "PENDIENTE",
+  },
+  {
+    id: 2,
+    idCliente: "123",
+    servicio: "Luz",
+    periodo: "2025-12",
+    monto: 120.5,
+    estado: "PENDIENTE",
+  },
+  {
+    id: 3,
+    idCliente: "456",
+    servicio: "Internet",
+    periodo: "2025-11",
+    monto: 210,
+    estado: "PAGADO",
+  },
+  {
+    id: 4,
+    idCliente: "456",
+    servicio: "Luz",
+    periodo: "2025-12",
+    monto: 98.3,
+    estado: "PENDIENTE",
+  },
+];
---
+++ src/api/facturas.api.ts
@@ -0,0 +1,26 @@
+import type { Factura } from "../types/factura";
+import { FACTURAS_DATA } from "./datos";
+
+const demoraSimulada = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+export async function obtenerFacturasPorIdCliente(
+  idCliente: string,
+): Promise<Factura[]> {
+  await demoraSimulada(700);
+
+  if (idCliente === "999") {
+    throw new Error("Error simulado de API para pruebas.");
+  }
+
+  return FACTURAS_DATA.filter((f) => f.idCliente === idCliente);
+}
+
+export async function pagarFacturaMock(idFactura: number): Promise<void> {
+  await demoraSimulada(600);
+
+  if (idFactura === -1) {
+    throw new Error(
+      "No se pudo procesar el pago, intente otra vez en unos minutos",
+    );
+  }
+}
